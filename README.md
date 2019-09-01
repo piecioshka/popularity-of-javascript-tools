2614f6c2412a5bc424744086a47292b66a575839
Piotr Kowalski <piecioshka@gmail.com>
2019-09-01 20:30:30 +0200

    Refresh popularity

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Popularity of JavaScript frameworks
 
-> Updated: 2018-07-23
+> Updated: 2019-09-01
 
 ## Open a report :bookmark:
 
@@ -8,11 +8,13 @@
 
 ## Popularity with GitHub stars :star2:
 
-* Vue.js - 108 346
-* React.js - 107 014
-* Angular - 38 720
-* Backbone.js - 27 229
-* Aurelia - 10 577
+* [Vue.js](https://github.com/vuejs/vue) - 147 229
+* [React.js](https://github.com/facebook/react) - 135 316
+* [Angular](https://github.com/angular/angular) - 50 896
+* [Backbone.js](https://github.com/jashkenas/backbone) - 27 528
+* [Aurelia](https://github.com/aurelia/framework) - 11 114
+* [Svelte](https://github.com/sveltejs/svelte) - 22 890
+* [HyperApp](https://github.com/jorgebucaran/hyperapp) - 16 894
 
 ## License
 
--- src/fixtures/data.js
+++ src/fixtures/data.js
@@ -1,7 +1,9 @@
 const CHART_DATA = [
-    { value: 108346, backgroundColor: '#4dba87', label: 'Vue.js' },
-    { value: 107014, backgroundColor: '#53c1de', label: 'React.js' },
-    { value: 38720, backgroundColor: '#c3002f', label: 'Angular' },
-    { value: 27229, backgroundColor: '#002a41', label: 'Backbone.js' },
-    { value: 10577, backgroundColor: '#c9107d', label: 'Aurelia' }
+    { value: 147229, backgroundColor: '#4dba87', label: 'Vue.js' },
+    { value: 135316, backgroundColor: '#53c1de', label: 'React.js' },
+    { value: 50896, backgroundColor: '#c3002f', label: 'Angular' },
+    { value: 27528, backgroundColor: '#002a41', label: 'Backbone.js' },
+    { value: 22890, backgroundColor: '#ff9800', label: 'Svelte' },
+    { value: 16894, backgroundColor: '#3d98c9', label: 'HyperApp' },
+    { value: 11114, backgroundColor: '#c9107d', label: 'Aurelia' }
 ];
