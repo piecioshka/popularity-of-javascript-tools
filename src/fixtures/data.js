6049a8f54dd7bfa1cb1c8ee7bdc63a800a52c490
Mateusz Obara <mateuszobara02@gmail.com>
2020-03-31 20:12:22 +0200

    Change page design

--- src/fixtures/data.js
+++
@@ -1,9 +0,0 @@
-const CHART_DATA = [
-    { value: 147229, backgroundColor: '#4dba87', label: 'Vue.js' },
-    { value: 135316, backgroundColor: '#53c1de', label: 'React.js' },
-    { value: 50896, backgroundColor: '#c3002f', label: 'Angular' },
-    { value: 27528, backgroundColor: '#002a41', label: 'Backbone.js' },
-    { value: 22890, backgroundColor: '#ff9800', label: 'Svelte' },
-    { value: 16894, backgroundColor: '#3d98c9', label: 'HyperApp' },
-    { value: 11114, backgroundColor: '#c9107d', label: 'Aurelia' }
-];
